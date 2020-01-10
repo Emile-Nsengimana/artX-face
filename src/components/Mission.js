e836e33c9b1770054fe7b339bc723e8932c6e5d2
Emile-Nsengimana <emilereas7@gmail.com>
2020-01-10 10:17:52 +0200

    navbar
     - create a navigation bar
     - add app logo

---
+++ src/components/Mission.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackgroundImg from '../img/home.jpg';
+
+const Mission = () => {
+  return (
+    <>
+      <img className="slide" src={BackgroundImg} alt="" />
+    </>
+  );
+};
+
+export default Mission;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../img/logo.svg';
 
 const NavBar = () => (
   <div className="navbar">
-    <div className="container">
-      <div className="left-side">
-        <Link to="/home">HOME</Link>
-        <Link to="/gallery">GALLERY</Link>
-        <Link to="/about">ABOUT US</Link>
-      </div>
-      <div className="right-menu">
-        <button type="button">Login</button>
-        <button type="button">Get Started</button>
-      </div>
+    <div className="logo">
+      <img src={Logo} alt="" />
+    </div>
+    <div className="left-menu">
+      <Link to="/">HOME</Link>
+      <Link to="/gallery">GALLERY</Link>
+      <Link to="/about">ABOUT US</Link>
+    </div>
+    <div className="right-menu">
+      <button type="button">Login</button>
+      <button type="button" className="btn-round">Get Started</button>
     </div>
   </div>
 );
